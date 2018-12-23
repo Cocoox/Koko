67d6df176b6392927c4386757ee5627048eb0733
Cocoox <ShockKing43@gmail.com>
2018-12-23 21:09:52 +0200

    Cool update

--- mods/Fun.py
+++ mods/Fun.py
@@ -4,8 +4,10 @@ import discord, asyncio
 from discord.ext import commands
 
 import random
-import urbandict
 import logging
+import re
+import urbandict
+import brainyquote
 
 log = logging.getLogger('koko')
 
@@ -15,12 +17,12 @@ class Fun:
 
     @commands.command(pass_context=True, aliases=['coinflip'])
     async def coin(self, ctx):
-        ans = ['Heads!','Tails!']
-        ans = random.choice(ans)
-        await self.bot.send_message(ctx.message.channel, ans)
+        answer = ['Heads!','Tails!']
+        answer = random.choice(answer)
+        await self.bot.send_message(ctx.message.channel, answer)
 
     @commands.command(pass_context=True, aliases=['urb'])
-    async def urban(self, ctx, word:str):
+    async def urban(self, ctx, *, word:str):
         """Get a definition from the urban dictionary."""
         try:
             urban = urbandict.define(word)
@@ -32,6 +34,27 @@ class Fun:
         message += '__Definition__ \n{0}\n\n'.format(urban[0]['def'])
         message += '__Example__ \n{0}'.format(urban[0]['example'])
         await self.bot.shorten(ctx.message.channel, message)
+    
+    @commands.command(pass_context=True)
+    async def quote(self, ctx, topic:str=None):
+        if topic is None:
+            quote = brainyquote.pybrainyquote.get_random_quote()
+        else:
+            quote = brainyquote.pybrainyquote.get_quotes(topic.lower())
+        try:
+            quote = str(quote[0])
+        except IndexError:
+            log.error('InvalidTopic, user informed')
+            await self.bot.send_message(ctx.message.channel, 'Topic not found.')
+            return
+        if quote.startswith('<img'):
+            regex = re.compile(r'(url=")(.+)("\sid=)')
+            match = regex.findall(quote)
+            url = match[0][1]
+            link = 'https://www.brainyquote.com/' + url
+            await self.bot.send_message(ctx.message.channel, link)
+            return
+        await self.bot.send_message(ctx.message.channel, '*"{}"*'.format(quote))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
