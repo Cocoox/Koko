b929318820fd020aad55c2f3b1d5842d2b52f988
Cocoox <ShockKing43@gmail.com>
2018-12-23 21:10:56 +0200

    Cool update

--- bot.py
+++ bot.py
@@ -6,10 +6,8 @@ import logging
 logging.basicConfig(format='[%(asctime)s][%(levelname)s] : %(message)s', level=logging.INFO)
 log = logging.getLogger('koko')
 
-token = 'NTE4NzQ3NzY4MzQ5NjU1MDUw.DvwKKA.s-NqXlKNT5Q-RtPC-Ei1O5CSN_M'
-
-# with open("token.txt", "r") as tokenfile:
-#     token = tokenfile.read()
+with open("token.txt", "r") as tokenfile:
+    token = tokenfile.read()
 
 bot = Koko(token=token)
 log.info('starting bot')
